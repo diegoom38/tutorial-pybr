c2280622e1aa5f47220f2c5aabb53809414a8d43
diego.oliveira <diego.oliveira@streamshop.com.br>
2024-05-02 18:17:36 -0300

    Adiciona integração contínua

--- api_pedidos/api.py
+++ api_pedidos/api.py
@@ -1,50 +1,70 @@
 from http import HTTPStatus
 from urllib.request import Request
 from uuid import UUID
+
 from fastapi import Depends, FastAPI
 from fastapi.responses import JSONResponse
+
 from api_pedidos.esquema import ErrorResponse, HealthCheckResponse, Item
-from api_pedidos.excecao import PedidoNaoEncontradoError, FalhaDeComunicacaoError
+from api_pedidos.excecao import (
+    FalhaDeComunicacaoError,
+    PedidoNaoEncontradoError,
+)
 
 app = FastAPI()
 
+
 @app.get(
-        "/healthcheck", 
-        tags=["healthcheck"], 
-        summary="Integridade do sistema", 
-        description="Checa se o servidor está online", 
-        response_model=HealthCheckResponse
-    )
+    "/healthcheck",
+    tags=["healthcheck"],
+    summary="Integridade do sistema",
+    description="Checa se o servidor está online",
+    response_model=HealthCheckResponse,
+)
 async def healthcheck():
     return HealthCheckResponse(status="ok")
+
 
 def recuperar_itens_por_pedido(identificacao_do_pedido: UUID) -> list[Item]:
     pass
 
+
 @app.get(
-        "/orders/{identificacao_do_pedido}/items",
-        responses={
-            HTTPStatus.NOT_FOUND.value: {
-                "description": "Pedido não encontrado",
-                "model": ErrorResponse,
-            },
-            HTTPStatus.BAD_GATEWAY.value: {
-                "description": "Falha de comunicação com o servidor remoto",
-                "model": ErrorResponse,
-            }
+    "/orders/{identificacao_do_pedido}/items",
+    responses={
+        HTTPStatus.NOT_FOUND.value: {
+            "description": "Pedido não encontrado",
+            "model": ErrorResponse,
         },
-        tags=["pedidos"], 
-        summary="Itens de um pedido", 
-        description="Retorna todos os itens de um determinado pedido",
-        response_model=list[Item]
-    )
+        HTTPStatus.BAD_GATEWAY.value: {
+            "description": "Falha de comunicação com o servidor remoto",
+            "model": ErrorResponse,
+        },
+    },
+    tags=["pedidos"],
+    summary="Itens de um pedido",
+    description="Retorna todos os itens de um determinado pedido",
+    response_model=list[Item],
+)
 def listar_itens(itens: list[Item] = Depends(recuperar_itens_por_pedido)):
     return itens
 
+
 @app.exception_handler(PedidoNaoEncontradoError)
-def tratar_erro_pedido_nao_encontrado(request: Request, exc: PedidoNaoEncontradoError):
-    return JSONResponse(status_code=HTTPStatus.NOT_FOUND, content={"message": "Pedido não encontrado"})
+def tratar_erro_pedido_nao_encontrado(
+    request: Request, exc: PedidoNaoEncontradoError
+):
+    return JSONResponse(
+        status_code=HTTPStatus.NOT_FOUND,
+        content={"message": "Pedido não encontrado"},
+    )
+
 
 @app.exception_handler(FalhaDeComunicacaoError)
-def tratar_erro_falha_de_comunicacao(request: Request, exc: FalhaDeComunicacaoError):
-    return JSONResponse(status_code=HTTPStatus.BAD_GATEWAY, content={"message": "Falha de comunicação com o servidor remoto"})
+def tratar_erro_falha_de_comunicacao(
+    request: Request, exc: FalhaDeComunicacaoError
+):
+    return JSONResponse(
+        status_code=HTTPStatus.BAD_GATEWAY,
+        content={"message": "Falha de comunicação com o servidor remoto"},
+    )
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,16 @@
 from http import HTTPStatus
+from uuid import UUID
 
 import pytest
-from api_pedidos.api import app, recuperar_itens_por_pedido
 from fastapi.testclient import TestClient
-from uuid import UUID
+
+from api_pedidos.api import app, recuperar_itens_por_pedido
 from api_pedidos.esquema import Item
-from api_pedidos.excecao import FalhaDeComunicacaoError, PedidoNaoEncontradoError
+from api_pedidos.excecao import (
+    FalhaDeComunicacaoError,
+    PedidoNaoEncontradoError,
+)
+
 
 @pytest.fixture
 def cliente():
@@ -17,17 +22,16 @@ class TestHealthCheck:
         resposta = cliente.get("/healthcheck")
         assert resposta.status_code == HTTPStatus.OK
 
-
     def test_formato_de_retorno_deve_ser_json(self, cliente):
         resposta = cliente.get("/healthcheck")
         assert resposta.headers["Content-Type"] == "application/json"
-
 
     def test_deve_conter_informacoes(self, cliente):
         resposta = cliente.get("/healthcheck")
         assert resposta.json() == {
             "status": "ok",
         }
+
 
 @pytest.fixture
 def sobreescreve_recuperar_itens_por_pedido():
@@ -36,36 +40,60 @@ def sobreescreve_recuperar_itens_por_pedido():
             if isinstance(itens_ou_erro, Exception):
                 raise itens_ou_erro
             return itens_ou_erro
+
         app.dependency_overrides[recuperar_itens_por_pedido] = duble
+
     yield _sobreescreve_recuperar_itens_por_pedido
     app.dependency_overrides.clear()
 
 
 class TestListarPedidos:
-    def test_quando_identificacao_do_pedido_invalido_um_erro_deve_ser_retornado(self, cliente):
+    def test_quando_identificacao_do_pedido_invalido_um_erro_deve_ser_retornado(
+        self, cliente
+    ):
         resposta = cliente.get("/orders/valor-invalido/items")
         assert resposta.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
 
-
-    def test_quando_pedido_nao_encontrado_um_erro_deve_ser_retornado(self, cliente):
+    def test_quando_pedido_nao_encontrado_um_erro_deve_ser_retornado(
+        self, cliente
+    ):
         def duble(identificacao_do_pedido: UUID) -> list[Item]:
             raise PedidoNaoEncontradoError()
+
         app.dependency_overrides[recuperar_itens_por_pedido] = duble
-        resposta = cliente.get("/orders/ea78b59b-885d-4e7b-9cd0-d54acadb4933/items")
+        resposta = cliente.get(
+            "/orders/ea78b59b-885d-4e7b-9cd0-d54acadb4933/items"
+        )
         assert resposta.status_code == HTTPStatus.NOT_FOUND
 
-
-    def test_quando_encontrar_pedido_codigo_ok_deve_ser_retornado(self, cliente):
+    def test_quando_encontrar_pedido_codigo_ok_deve_ser_retornado(
+        self, cliente
+    ):
         def duble(identificacao_do_pedido: UUID) -> list[Item]:
             return []
+
         app.dependency_overrides[recuperar_itens_por_pedido] = duble
-        resposta = cliente.get("/orders/7e290683-d67b-4f96-a940-44bef1f69d21/items")
+        resposta = cliente.get(
+            "/orders/7e290683-d67b-4f96-a940-44bef1f69d21/items"
+        )
         assert resposta.status_code == HTTPStatus.OK
 
     def test_quando_encontrar_pedido_deve_retornar_itens(self, cliente):
         itens_esperados = [
-            Item(sku="1", description="Item 1", image_url="http://url.com/img1", reference="ref1", quantity=1),
-            Item(sku="2", description="Item 2", image_url="http://url.com/img2", reference="ref2", quantity=2),
+            Item(
+                sku="1",
+                description="Item 1",
+                image_url="http://url.com/img1",
+                reference="ref1",
+                quantity=1,
+            ),
+            Item(
+                sku="2",
+                description="Item 2",
+                image_url="http://url.com/img2",
+                reference="ref2",
+                quantity=2,
+            ),
         ]
 
         # Definindo a função de simulação (mock)
@@ -76,7 +104,9 @@ class TestListarPedidos:
         app.dependency_overrides[recuperar_itens_por_pedido] = duble
 
         # Fazendo a requisição à rota
-        resposta = cliente.get("/orders/7e290683-d67b-4f96-a940-44bef1f69d21/items")
+        resposta = cliente.get(
+            "/orders/7e290683-d67b-4f96-a940-44bef1f69d21/items"
+        )
 
         # Convertendo a resposta para uma lista de objetos Item
         itens_resposta = [Item(**item) for item in resposta.json()]
@@ -84,7 +114,11 @@ class TestListarPedidos:
         # Verificando se a resposta corresponde aos itens esperados
         assert itens_resposta == itens_esperados
 
-    def test_quando_fonte_de_pedidos_falha_um_erro_deve_ser_retornado(self, cliente, sobreescreve_recuperar_itens_por_pedido):
+    def test_quando_fonte_de_pedidos_falha_um_erro_deve_ser_retornado(
+        self, cliente, sobreescreve_recuperar_itens_por_pedido
+    ):
         sobreescreve_recuperar_itens_por_pedido(FalhaDeComunicacaoError())
-        resposta = cliente.get("/orders/ea78b59b-885d-4e7b-9cd0-d54acadb4933/items")
+        resposta = cliente.get(
+            "/orders/ea78b59b-885d-4e7b-9cd0-d54acadb4933/items"
+        )
         assert resposta.status_code == HTTPStatus.BAD_GATEWAY
