e6914e6f260948bbccf18f4969c7e0280431c561
diego.oliveira <diego.oliveira@streamshop.com.br>
2024-05-02 18:05:33 -0300

    Adiciona documentação ao projeto

--- api_pedidos/api.py
+++ api_pedidos/api.py
@@ -3,19 +3,41 @@ from urllib.request import Request
 from uuid import UUID
 from fastapi import Depends, FastAPI
 from fastapi.responses import JSONResponse
-from api_pedidos.esquema import Item
+from api_pedidos.esquema import ErrorResponse, HealthCheckResponse, Item
 from api_pedidos.excecao import PedidoNaoEncontradoError, FalhaDeComunicacaoError
 
 app = FastAPI()
 
-@app.get("/healthcheck")
+@app.get(
+        "/healthcheck", 
+        tags=["healthcheck"], 
+        summary="Integridade do sistema", 
+        description="Checa se o servidor está online", 
+        response_model=HealthCheckResponse
+    )
 async def healthcheck():
-    return {"status": "ok"}
+    return HealthCheckResponse(status="ok")
 
 def recuperar_itens_por_pedido(identificacao_do_pedido: UUID) -> list[Item]:
     pass
 
-@app.get("/orders/{identificacao_do_pedido}/items")
+@app.get(
+        "/orders/{identificacao_do_pedido}/items",
+        responses={
+            HTTPStatus.NOT_FOUND.value: {
+                "description": "Pedido não encontrado",
+                "model": ErrorResponse,
+            },
+            HTTPStatus.BAD_GATEWAY.value: {
+                "description": "Falha de comunicação com o servidor remoto",
+                "model": ErrorResponse,
+            }
+        },
+        tags=["pedidos"], 
+        summary="Itens de um pedido", 
+        description="Retorna todos os itens de um determinado pedido",
+        response_model=list[Item]
+    )
 def listar_itens(itens: list[Item] = Depends(recuperar_itens_por_pedido)):
     return itens
 
